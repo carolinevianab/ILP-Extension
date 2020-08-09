4b89dc249aae012235894e3b128bd86e315f3dc1
carolinevianab <carolinevb123@gmail.com>
2020-08-08 22:22:50 -0300

    Things I forgot to commit

--- ILP-Extension/ILP-Extension/ViewController.swift
+++ ILP-Extension/ILP-Extension/ViewController.swift
@@ -29,6 +29,16 @@ class ViewController: UIViewController {
         bntHavyion.addTarget(self, action: #selector(clickButton), for: .touchUpInside)
         bntGlixino.addTarget(self, action: #selector(clickButton), for: .touchUpInside)
         bntiEye.addTarget(self, action: #selector(clickButton), for: .touchUpInside)
+        
+        bntPinclet.layer.borderWidth = 1
+        bntPinclet.layer.cornerRadius = 20
+        bntHavyion.layer.borderWidth = 1
+        bntHavyion.layer.cornerRadius = 20
+        bntGlixino.layer.borderWidth = 1
+        bntGlixino.layer.cornerRadius = 20
+        bntiEye.layer.borderWidth = 1
+        bntiEye.layer.cornerRadius = 20
+        
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -42,6 +52,29 @@ class ViewController: UIViewController {
     @objc func clickButton(_ sender: UIButton){
         self.clicked = sender.restorationIdentifier!
         runcount = 0
+        bntPinclet.isHidden = true
+        bntHavyion.isHidden = true
+        bntGlixino.isHidden = true
+        bntiEye.isHidden = true
+        
+        switch self.clicked {
+        case "Pinclet":
+            bntPinclet.isHidden = false
+            break
+        case "Havyion":
+            bntHavyion.isHidden = false
+            break
+        case "Glixino":
+            bntGlixino.isHidden = false
+            break
+        case "iEye":
+            bntiEye.isHidden = false
+            break
+        default:
+            break
+        }
+        
+        
         if let scene = self.scene {
             let timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { timer in
                 scene.animateCreature(self.clicked)
@@ -50,6 +83,10 @@ class ViewController: UIViewController {
                 if self.runcount >= 20{
                     timer.invalidate()
                     self.performSegue(withIdentifier: "ToDetails", sender: sender)
+                    self.bntPinclet.isHidden = false
+                    self.bntHavyion.isHidden = false
+                    self.bntGlixino.isHidden = false
+                    self.bntiEye.isHidden = false
                     scene.removeAllChildren()
                 }
                 
